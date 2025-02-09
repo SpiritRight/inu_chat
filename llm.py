77cce16f0a973737f2584451b2026284f4368642
J_sin <jungsinwoo@gmail.com>
2025-02-09 12:00:41 +0900

    데이터 추가

--- llm.py
+++ llm.py
@@ -9,6 +9,7 @@ from langchain_upstage import UpstageEmbeddings
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.llms import Ollama
 
 from config import answer_examples
 
@@ -25,7 +26,8 @@ def get_retriever():
     # embedding = OpenAIEmbeddings(model='text-embedding-3-large')
     embedding = UpstageEmbeddings(model='solar-embedding-1-large')
     # index_name = 'tax-table-index'
-    database = Chroma(collection_name='chroma-inu-new', persist_directory="./chroma_inu-new", embedding_function=embedding)
+    # database = Chroma(collection_name='chroma-inu-new', persist_directory="./chroma_inu-new", embedding_function=embedding) # 학칙만
+    database = Chroma(collection_name='chroma-inu', persist_directory="./chroma_inu", embedding_function=embedding) #학칙+장학금
     retriever = database.as_retriever(search_kwargs={'k': 4})
     return retriever
 
@@ -55,13 +57,14 @@ def get_history_retriever():
     return history_aware_retriever
 
 
-def get_llm(model='gpt-4o'):
+def get_llm(model='gpt-4o-mini'):
     llm = ChatOpenAI(model=model)
+    # llm = Ollama(model=model)
     return llm
 
 
 def get_dictionary_chain():
-    dictionary = ["사람을 나타내는 표현 -> 거주자"]
+    dictionary = ["사람을 나타내는 표현 -> 학생"]
     llm = get_llm()
     prompt = ChatPromptTemplate.from_template(f"""
         사용자의 질문을 보고, 우리의 사전을 참고해서 사용자의 질문을 변경해주세요.
@@ -90,11 +93,12 @@ def get_rag_chain():
         examples=answer_examples,
     )
     system_prompt = (
-        "당신은 인천대학교 학칙 전문가입니다. 사용자의 인천대학교에 관한 질문에 답변해주세요"
+        "당신은 인천대학교 학칙과 장학금 전문가입니다. 사용자의 인천대학교에 관한 질문에 답변해주세요"
         "아래에 제공된 문서를 활용해서 답변해주시고"
         "답변을 알 수 없다면 모른다고 답변해주세요"
-        "답변을 제공할 때는 학칙 (XX조)에 따르면 이라고 시작하면서 답변해주시고"
-        "3-4 문장정도의 짧은 내용의 답변을 원합니다"
+        "답변을 제공할 때는 학칙 (XX조)에 따르면 이라고 시작하면서 답변해주세요"
+        "단 장학금에 관련된 내용일 경우에는 학칙 (XX조)를 제외하고 말해주세요"
+        # "3-4 문장정도의 짧은 내용의 답변을 원합니다"
         "\n\n"
         "{context}"
     )
