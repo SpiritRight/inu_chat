f646c7e0355cf2f0641724c6c0e54931cdf3711b
J_sin <jungsinwoo@gmail.com>
2025-02-09 16:42:19 +0900

    데이터 추가 + 프롬프트 추가

--- llm.py
+++ llm.py
@@ -3,7 +3,7 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, FewS
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_openai import ChatOpenAI
-from langchain_openai import OpenAIEmbeddings
+# from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
 from langchain_upstage import UpstageEmbeddings
 from langchain_community.chat_message_histories import ChatMessageHistory
@@ -19,15 +19,16 @@ store = {}
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
     if session_id not in store:
         store[session_id] = ChatMessageHistory()
+        print(store)
     return store[session_id]
 
 
 def get_retriever():
     # embedding = OpenAIEmbeddings(model='text-embedding-3-large')
     embedding = UpstageEmbeddings(model='solar-embedding-1-large')
-    # index_name = 'tax-table-index'
+    # index_name = 'law-table-index'
     # database = Chroma(collection_name='chroma-inu-new', persist_directory="./chroma_inu-new", embedding_function=embedding) # 학칙만
-    database = Chroma(collection_name='chroma-inu', persist_directory="./chroma_inu", embedding_function=embedding) #학칙+장학금
+    database = Chroma(collection_name='chroma-inu-plus', persist_directory="./chroma_inu-plus", embedding_function=embedding) #학칙+장학금
     retriever = database.as_retriever(search_kwargs={'k': 4})
     return retriever
 
@@ -93,11 +94,17 @@ def get_rag_chain():
         examples=answer_examples,
     )
     system_prompt = (
-        "당신은 인천대학교 학칙과 장학금 전문가입니다. 사용자의 인천대학교에 관한 질문에 답변해주세요"
+        "당신은 인천대학교에 관련된 전문가입니다. 사용자의 인천대학교에 관한 질문에 답변해주세요"
         "아래에 제공된 문서를 활용해서 답변해주시고"
         "답변을 알 수 없다면 모른다고 답변해주세요"
         "답변을 제공할 때는 학칙 (XX조)에 따르면 이라고 시작하면서 답변해주세요"
-        "단 장학금에 관련된 내용일 경우에는 학칙 (XX조)를 제외하고 말해주세요"
+        "장학금에 관련된 내용일 경우에는 '학칙에 따르면'을 제거하고 말해주세요"
+        # "세부 사항을 이야기할 때 <br>과 같은 mark를 제거하고 말해주세요."
+        # "(1),(2)와 같이 문단을 나눌 수 있다면 나눠서 보기 편하게 보여주세요."
+        "(대학명) (학과명) 졸업학점은 (숫자)학점이다. 로 패턴을 고정해줘"
+        "가능한 자세하게 답변을 해주세요."
+        "XX학번이라고 하면 (20)XX학년 입학이라 생각해면 돼."
+        "졸업요건을 물어볼 때 (대학)을 말하지 않고 학과만 말해도 (대학)은 너가 알아서 찾아서 넣어줘."
         # "3-4 문장정도의 짧은 내용의 답변을 원합니다"
         "\n\n"
         "{context}"
@@ -130,8 +137,8 @@ def get_rag_chain():
 def get_ai_response(user_message):
     dictionary_chain = get_dictionary_chain()
     rag_chain = get_rag_chain()
-    tax_chain = {"input": dictionary_chain} | rag_chain
-    ai_response = tax_chain.stream(
+    raw_chain = {"input": dictionary_chain} | rag_chain
+    ai_response = raw_chain.stream(
         {
             "question": user_message
         },
